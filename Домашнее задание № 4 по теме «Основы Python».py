c22d69291f3d74bf6d4762dcfbe6a01ed6447fdc
Kaminskiy Dmitriy <demonidc@gmail.com>
2020-10-08 19:39:15 +0600

    Update Домашнее задание № 4 по теме «Основы Python».py

--- Домашнее задание № 4 по теме «Основы Python».py
+++ Домашнее задание № 4 по теме «Основы Python».py
@@ -1,4 +1,12 @@
-print('Hello')
+width = 10
+length = 35
+height = 5
 
-99999999
-88888888
+if width < 15 and length < 15 and height < 15:
+    print ("Коробка № 1")
+elif width > 15 and width < 50:
+    print('Коробка № 2')
+elif length > 15 and length < 50:
+    print('Коробка № 2')
+elif height > 15 and height < 50:
+    print('Коробка № 2')
